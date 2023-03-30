04086570fffdf6e60c93c45c1d2e3f44da6eb5ee
dunsd <dthomasparker@gmail.com>
2023-03-30 20:52:34 +0100

    Add mod functions to handle wrapping and tests

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,18 +1,26 @@
 function caesarCipher(string, key) { //97-122 = lowercase, 65-90 = uppercase
     
     let codeArr = string.split("").map(x => x.charCodeAt());//.join(" ");
-    codeArr.forEach(char => {
-        if(char > 96 && char < 123) {
-            char = char += key;
-            //console.log(char);
+    for(let i=0;i<codeArr.length;i+=1){
+        if((codeArr[i] > 96 && codeArr[i] < 123)) {
+            codeArr[i] = modLower(codeArr[i],key);
         }
-        
-    });
+        else if((codeArr[i] > 64 && codeArr[i] < 91)) {
+            codeArr[i] = modUpper(codeArr[i],key);
+        }
+        codeArr[i] = String.fromCharCode(codeArr[i]);
+    }
+    console.log(modLower(122,1));
     return codeArr.join("");
 }
 
-function mod() {
-    
+//Handle wrapping in range
+function modLower(index, key) { 
+    return ((index-97+key)%26)+97;
+}
+
+function modUpper(index, key) { 
+    return ((index-65+key)%26)+65;
 }
 
 export default caesarCipher;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,5 +1,17 @@
 import caesarCipher from "./caesarCipher";
 
 test("shifts lowercase string", () => {
-    expect(caesarCipher("abcdefghijklmnopqrstuvwxyz", 1)).toBe("bcdefghijklmnopqrstuvwxyza");
+    expect(caesarCipher("abcdefghijklmnopqrstuvwxy", 1)).toBe("bcdefghijklmnopqrstuvwxyz");
+})
+
+test("allows wrapping", () => {
+    expect(caesarCipher("xyz", 1)).toBe("yza");
+})
+
+test("handles uppercase", () => {
+    expect(caesarCipher("ABBDccd", 3)).toBe("DEEGffg");
+})
+
+test("handles punctuation", () => {
+    expect(caesarCipher("abc$ >.$",1)).toBe("bcd$ >.$")
 })
